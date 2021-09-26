08a6d8238c7661a57d526e1a5d0f6ceb32a39867
Michal Gniadek <mgniadek@interia.pl>
2021-09-26 12:28:39 +0200

    Cleanup tests, check occurrences and flags in UseEquals

--- src/app_state/tests.rs
+++ src/app_state/tests.rs
@@ -18,8 +18,8 @@ struct ForbidEmpty {
 fn forbid_empty() {
     test_app(
         |args| {
-            args[0].enter_value("a");
-            args[2].enter_value("");
+            args[0].enter("a");
+            args[2].enter("");
         },
         ForbidEmpty {
             optional_no_empty1: Some("a".into()),
@@ -40,7 +40,7 @@ struct OptionalAndDefault {
 #[test]
 fn optional_and_default() {
     test_app(
-        |args| args[0].enter_value("a"),
+        |args| args[0].enter("a"),
         OptionalAndDefault {
             required: "a".into(),
             optional: None,
@@ -61,17 +61,20 @@ struct UseEquals {
     choose: String,
     #[clap(long, require_equals = true, multiple_occurrences = true)]
     multiple: Vec<String>,
+    #[clap(long, parse(from_occurrences))]
+    occurrences: i32,
+    #[clap(long)]
+    flag: bool,
 }
 
 #[test]
 fn use_equals() {
     test_app(
         |args| {
-            args[0].enter_value("a");
-            args[1].enter_value("b");
-            args[2].enter_value("c");
-            args[3].enter_value("P");
-            args[4].enter_values(&["d", "e"]);
+            enter_consecutive(args, ["a", "b", "c", "P"]);
+            args[4].enter_multiple(["d", "e"]);
+            args[5].occurrences(3);
+            args[6].set();
         },
         UseEquals {
             long: "a".into(),
@@ -79,6 +82,8 @@ fn use_equals() {
             path: "c".into(),
             choose: "P".into(),
             multiple: vec!["d".into(), "e".into()],
+            occurrences: 3,
+            flag: true,
         },
     );
 }
@@ -98,23 +103,45 @@ where
     assert_eq!(c, expected);
 }
 
+fn enter_consecutive<const N: usize>(args: &mut Vec<ArgState>, vals: [&str; N]) {
+    for i in 0..N {
+        args[i].enter(vals[i]);
+    }
+}
+
 impl crate::arg_state::ArgState {
-    fn enter_value(&mut self, val: &str) {
-        match &mut self.kind {
-            ArgKind::String { value, .. } => value.0 = val.to_string(),
-            _ => panic!("Called enter_value on {:?}", self),
+    fn enter(&mut self, val: &str) {
+        if let ArgKind::String { value, .. } = &mut self.kind {
+            value.0 = val.to_string();
+        } else {
+            panic!("Called enter on {:?}", self)
         }
     }
 
-    fn enter_values(&mut self, vals: &[&str]) {
-        match &mut self.kind {
-            ArgKind::MultipleStrings { values, .. } => {
-                *values = vals
-                    .iter()
-                    .map(|s| (s.to_string(), Uuid::new_v4()))
-                    .collect()
-            }
-            _ => panic!("Called enter_value on {:?}", self),
+    fn enter_multiple<const N: usize>(&mut self, vals: [&str; N]) {
+        if let ArgKind::MultipleStrings { values, .. } = &mut self.kind {
+            *values = vals
+                .iter()
+                .map(|s| (s.to_string(), Uuid::new_v4()))
+                .collect()
+        } else {
+            panic!("Called enter_multiple on {:?}", self)
+        }
+    }
+
+    fn occurrences(&mut self, val: i32) {
+        if let ArgKind::Occurences(i) = &mut self.kind {
+            *i = val;
+        } else {
+            panic!("Called occurrences on {:?}", self)
+        }
+    }
+
+    fn set(&mut self) {
+        if let ArgKind::Bool(b) = &mut self.kind {
+            *b = true;
+        } else {
+            panic!("Called set on {:?}", self)
         }
     }
 }
